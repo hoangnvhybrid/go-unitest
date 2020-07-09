e1bd0bcbbae9abf82e64484bd518ad9e1626556c
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-07-09 11:45:34 +0700

    gomock example

---
+++ hellomock/company_test.go
@@ -0,0 +1,15 @@
+package hellomock
+
+import (
+	"github.com/golang/mock/gomock"
+	"testing"
+)
+
+func TestCompany_Meeting(t *testing.T) {
+	person := NewPerson("Nguyen Hoang")
+	company := NewCompany(person)
+	t.Log(company.Meeting("hoa"))
+}
+func TestCompany_Meeting2(t *testing.T) {
+	//clt := gomock.
+}
